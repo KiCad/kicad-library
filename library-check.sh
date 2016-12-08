19eb32856b41964af9926544077088b17bdbf659
Marsfan <Marsfan@users.noreply.github.com>
2016-12-08 11:05:51 -0600

    add change comparasion tool

---
+++ library-check.sh
@@ -0,0 +1,6 @@
+for OUTPUT in $(ls ./library/*.lib | wc -l)
+do
+	files=(./library/*.lib)
+	file =  echo"${files[n]}"
+	python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$file --original /usr/local/share/kicad-library/library/$file
+done
