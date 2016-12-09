e3f4ca2d69994d2f04ee8ced99e283849d7a4209
Marsfan <Marsfan@users.noreply.github.com>
2016-12-08 20:23:12 -0600

    more testing

--- library-check.sh
+++ library-check.sh
@@ -3,5 +3,5 @@ do
 	files=(./library/*.lib)
 	echo"${files[OUTPUT]}"
 	echo $file
-	echo $python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$file --original /usr/local/share/kicad-library/library/$file
+	echo $python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/${files[OUTPUT]} --original /usr/local/share/kicad-library/library/${files[OUTPUT]}
 done
