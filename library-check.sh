d022140221b0695a3d956ff9e6fd71c7eccc38ca
Marsfan <Marsfan@users.noreply.github.com>
2016-12-09 11:41:42 -0600

    another text

--- library-check.sh
+++ library-check.sh
@@ -1,5 +1,5 @@
 i=0
 for file in *; do
 	files[$i]$file
-	echo ${python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$files --original /usr/local/share/kicad-library/library/$file
+	echo ${python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$files --original /usr/local/share/kicad-library/library/$file}
 done
