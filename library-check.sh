2d316f0170fed991a54a6a954c38c8df45c681c1
Marsfan <Marsfan@users.noreply.github.com>
2016-12-09 13:14:19 -0600

    more testing

--- library-check.sh
+++ library-check.sh
@@ -1,5 +1,4 @@
 i=0
 for file in ./library/*.lib; do
-	files[$i]$file
 	echo python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$file --original /usr/local/share/kicad-library/library/$file
 done
