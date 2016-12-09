cc5c63e021abbe342820c8881165610edad353e4
Marsfan <Marsfan@users.noreply.github.com>
2016-12-09 11:19:26 -0600

    redid shell code, trying again

--- library-check.sh
+++ library-check.sh
@@ -1,7 +1,5 @@
-for OUTPUT in $(ls ./library/*.lib | wc -l)
-do
-	files=(./library/*.lib)
-	echo"${files[OUTPUT]}"
-	echo $file
+
+for file in *; do
+	files = "$file"
 	echo ${python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/${files[OUTPUT]} --original /usr/local/share/kicad-library/library/${files[OUTPUT]}}
 done
