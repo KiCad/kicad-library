84803ddb736bb1c6560144d0d7b65137746dfa38
Marsfan <Marsfan@users.noreply.github.com>
2016-12-12 10:15:46 -0600

    we are on the last stretch

--- library-check.sh
+++ library-check.sh
@@ -1,5 +1,6 @@
 i=0
 for file in ./library/*.lib; do
-	python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$file --original /usr/local/share/kicad-library/library/$file
-	python /usr/local/share/kicad-library-utils/schlib/checklib.py -v ./library/*
+	for item in sudo python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$file --original /usr/local/share/kicad-library/library/$file do
+		sudo python /usr/local/share/kicad-library-utils/schlib/checklib.py -c $item
+
 done
