4c6475928af0630a4decc46ffd371255a71f0831
Marsfan <Marsfan@users.noreply.github.com>
2016-12-12 10:29:25 -0600

    need to freaking call done twice

--- library-check.sh
+++ library-check.sh
@@ -2,5 +2,5 @@ i=0
 for file in ./library/*.lib; do
 	for item in sudo python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new ./library/$file --original /usr/local/share/kicad-library/library/$file; do
 		sudo python /usr/local/share/kicad-library-utils/schlib/checklib.py -c $item
-
+	done
 done
