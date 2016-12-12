41e60624118178759bf619955b6474119436bf10
Marsfan <Marsfan@users.noreply.github.com>
2016-12-12 13:23:54 -0600

    and its not

--- library-check.sh
+++ library-check.sh
@@ -1,5 +1,5 @@
 for file in ./library/*.lib; do
-	for item in $(python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new $file --original /usr/local/share/kicad-library/$file); do
+	for item in $(sudo python /usr/local/share/kicad-library-utils/schlib/comparelibs.py --new $file --original /usr/local/share/kicad-library/$file); do
 		echo $item
 		echo $file
 		sudo python /usr/local/share/kicad-library-utils/schlib/checklib.py $file -c $item
