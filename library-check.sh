2c22e323aa6be5dcff58026e28570cf1c603d1bf
Michiel Fierst <m.fierst@fierstdesign.nl>
2017-05-24 19:10:40 +0200

    Revert "Reverts unintended changes to library-check.sh"
    This reverts commit 670ad34b6716f2121f4ddff1ba74175c1af952ee.

--- library-check.sh
+++ library-check.sh
@@ -3,7 +3,7 @@ DATA=()
 for files in library/*.lib; do
 DATA+=($files)
 done
-cd /home/travis/build/kicad-library-utils/schlib
+cd ../../kicad-library-utils/schlib
 for file in "${DATA[@]}"; do
 	python3 comparelibs.py --check --new /$1/$file --old /home/travis/build/kicad-library-old/$file
 	error="$(($error+$?))"
